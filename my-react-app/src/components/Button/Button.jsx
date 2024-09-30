d344900e6c22474112e3c48bfff1409c9ce19fca
Денис Жилин <zilin.d@sm.local>
2024-09-30 17:53:42 +0300

    4.10 Statefull и Stateless

--- my-react-app/src/components/Button/Button.jsx
+++ my-react-app/src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import './Button.css';
 
 
-const Button = ({text}) => {
+const Button = ({text, onClick}) => {
 	
 	return (
 		<>
-			<button  className="button accent">{text}</button>
+			<button  className="button accent" onClick={onClick}>{text}</button>
 		</>
 	);
 };

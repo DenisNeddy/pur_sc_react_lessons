02f0e86a9ad645638a6143e5858ac765cf906214
Денис Жилин <zilin.d@sm.local>
2024-10-03 15:18:37 +0300

    6.3 CSS модули

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import './Header.css';
+import styles from './Header.module.css';
 
 const Header = () => {
 	return (
 		<div className="header">
-			<img className="logo" src="/logo.svg" alt="Логотип" />
+			<img className={styles.logo} src="/logo.svg" alt="Логотип" />
 		</div>
 	);
 };

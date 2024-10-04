22439666999075e5612e1138f5bc02fb19cd8829
Денис Жилин <zilin.d@sm.local>
2024-10-04 17:53:22 +0300

    7.11 Создание своего hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,7 +7,17 @@ import Header from './components/Header/Header.jsx';
 import JournalList from './components/JournalList/JournalList.jsx';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx';
 import JournalForm from './components/JournalForm/JournalForm.jsx';
-import { useEffect, useState } from 'react';
+import { useLocalStorage } from './hooks/use-localstorage.hook.js';
+
+function mapItems(items) {
+	if(!items) {
+		return [];
+	}
+	return items.map(i => ({
+		...i,
+		date: new Date(i.date)
+	}));
+}
 
 
 
@@ -28,28 +38,10 @@ console.log('муха');
 
 function App() {
 
-	const [ items, setItems] = useState([]);
-	useEffect(() => {
-
-		const data = JSON.parse(localStorage.getItem('data'));
-		if(data) {
-			setItems(data.map(item =>({
-				...item,
-				date: new Date(item.date)
-			})));
-		}
-	}, []);
-
-	useEffect(() => {
-		if(items.length) {
-			localStorage.setItem('data', JSON.stringify(items));
-		}
-		console.log(items);
-	}, [items]);
-
+	const [ items, setItems] = useLocalStorage('data');
 
 	const addItem = (item) => {
-		setItems(oldItems => [...oldItems, {...item, date: new Date(item.date) , id:oldItems.length > 0 ?  Math.max(...oldItems.map(i => i.id)) + 1 : 1}]);
+		setItems([...mapItems(items), {...item, date: new Date(item.date) , id:items.length > 0 ?  Math.max(...items.map(i => i.id)) + 1 : 1}]);
 	};
 
 	return (
@@ -57,7 +49,7 @@ function App() {
 		  <LeftPanel>			
 				<Header/>
 				<JournalAddButton />
-				<JournalList items={items} />
+				<JournalList items={mapItems(items)} />
 		  </LeftPanel>
 		  <Body>
 				<JournalForm onSubmit={addItem}/>

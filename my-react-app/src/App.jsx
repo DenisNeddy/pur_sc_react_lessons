a3472a6c700ba6eea312c4d1804d1172ca519be3
Денис Жилин <zilin.d@sm.local>
2024-10-04 13:21:25 +0300

    7.2 useEffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,28 +7,39 @@ import Header from './components/Header/Header.jsx';
 import JournalList from './components/JournalList/JournalList.jsx';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx';
 import JournalForm from './components/JournalForm/JournalForm.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
-const INITIAL_DATA = [
-	{   id: 1,
-		title: 'Подготовка к обновлению курсов',
-		text: 'Горные походы открывают удивительные природные ланшафты',
-		date: new Date()
-	},
-	{   id: 2,
-		title: 'Подготовка курсов 2.0',
-		text: 'что то еще очень важное я стал забывать',
-		date: new Date()
-	}
-];
+// const INITIAL_DATA = [
+// 	{   id: 1,
+// 		title: 'Подготовка к обновлению курсов',
+// 		text: 'Горные походы открывают удивительные природные ланшафты',
+// 		date: new Date()
+// 	},
+// 	{   id: 2,
+// 		title: 'Подготовка курсов 2.0',
+// 		text: 'что то еще очень важное я стал забывать',
+// 		date: new Date()
+// 	}
+// ];
 
 console.log('муха');
 
 function App() {
 
-	const [ items, setItems] = useState(INITIAL_DATA);
+	const [ items, setItems] = useState([]);
+	useEffect(() => {
+
+		const data = JSON.parse(localStorage.getItem('data'));
+		if(data) {
+			setItems(data.map(item =>({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
 
 	const addItem = (item) => {
 		setItems(oldItems => [...oldItems, {...item, date: new Date(item.date) , id:oldItems.length > 0 ?  Math.max(...oldItems.map(i => i.id)) + 1 : 1}]);

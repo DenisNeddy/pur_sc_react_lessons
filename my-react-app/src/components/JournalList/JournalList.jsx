2ae445373b8e20648c58e5c199d0d4d2d8adbe0a
Денис Жилин <zilin.d@sm.local>
2024-10-14 17:06:02 +0300

    9.8 useMemo

--- my-react-app/src/components/JournalList/JournalList.jsx
+++ my-react-app/src/components/JournalList/JournalList.jsx
@@ -1,15 +1,11 @@
 import './JournalList.css';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { UserContext } from '../../contenxt/user.context';
 
 const JournalList = ({items}) => {
 	const {userId} = useContext(UserContext);
-	if(items.length === 0) {
-		return <p>Записей пока нет, добавьте первую</p>;
-	}
-	
 	const sortItems = (a, b) => {
 		if(a.date < b.date) {
 			return 1;
@@ -17,12 +13,18 @@ const JournalList = ({items}) => {
 			return -1;
 		}
 	};
+	const filteredItems = useMemo(() => items.filter(el => el.userId === userId).sort(sortItems), [items, userId]);
+	if(items.length === 0) {
+		return <p>Записей пока нет, добавьте первую</p>;
+	}
+	
+
 
 
 	return (
 		<div className="journal-list"> 
 			{
-				items.filter(el => el.userId === userId).sort(sortItems).map(el => (
+				filteredItems.map(el => (
 					<CardButton key={el.id}> 
 						<JournalItem 
 							title={el.title}
